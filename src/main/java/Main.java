4b9643ff5afe17fed29ec269e062aa5458f5ddae
Валентин <valmak04@gmail.com>
2021-09-26 15:44:17 +0400

    Done 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer digit = sc.nextInt();
+        int digit = sc.nextInt();
         if (digit % 2 == 0 ) {
             System.out.println(digit + 2);
-        } else if(digit % 2 != 0) {
+        } else if(digit % 2 > 0) {
             System.out.print(++digit);
         }
     }
