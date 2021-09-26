1ec904432de13a7099d86eb2d97650dd538ec3f9
Валентин <valmak04@gmail.com>
2021-09-26 15:42:12 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        Integer digit = sc.nextInt();
+        if (digit % 2 == 0 ) {
+            System.out.println(digit + 2);
+        } else if(digit % 2 != 0) {
+            System.out.print(++digit);
+        }
     }
 }
